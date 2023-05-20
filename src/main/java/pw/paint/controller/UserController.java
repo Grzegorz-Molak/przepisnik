893b8aac1875ad9a5f1cdacf8ee5ab1fa6470850
Zuzanna <01158303@pw.edu.pl>
2023-05-20 11:02:40 +0200

    request body

--- src/main/java/pw/paint/controller/UserController.java
+++ src/main/java/pw/paint/controller/UserController.java
@@ -1,10 +1,7 @@
 package pw.paint.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pw.paint.DTOs.model.SignUpRequest;
 import pw.paint.DTOs.model.UserDto;
 import pw.paint.service.UserService;
@@ -18,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public String signUp(SignUpRequest signUpRequest){
+    public String signUp(@RequestBody SignUpRequest signUpRequest){
         userService.signup(signUpRequest);
         return "user added successfully";
     }
