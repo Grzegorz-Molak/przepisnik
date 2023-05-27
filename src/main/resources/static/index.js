f36723dda6a83a6ef1c60d266b519914694afa1f
Dorota Wlazlo <dori.wlazlo@gmail.com>
2023-05-27 23:32:59 +0200

    adding basic registration form

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("rejestracja").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("rejestracja").style.display = "none";
+}
+
