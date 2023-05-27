2a2314294fadc245c3c5f31c09f9e52472597bac
Zuzanna <01158303@pw.edu.pl>
2023-05-27 12:37:10 +0200

    Zamieniono w plikach repository na ObjectId klucz

--- src/main/java/pw/paint/repository/RecipeRepository.java
+++ src/main/java/pw/paint/repository/RecipeRepository.java
@@ -1,7 +1,8 @@
 package pw.paint.repository;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import pw.paint.model.Recipe;
 
-public interface RecipeRepository extends MongoRepository<Recipe, String> {
+public interface RecipeRepository extends MongoRepository<Recipe, ObjectId> {
 }
