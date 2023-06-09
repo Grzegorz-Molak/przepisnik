51f35a0686868d242dbf686076a38395b5e2a0cc
Dorota Wlazlo <dori.wlazlo@gmail.com>
2023-06-09 23:02:09 +0200

    Usuwanie zduplikowanego kodu do logowania i rejestracji

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -11,7 +11,6 @@ export class Recipe{
 }
 
 export function search(searchType,status){
-    const token = localStorage.getItem('jwtToken');
     let requestBody;
     if(searchType === "short"){
         requestBody = {
@@ -53,8 +52,7 @@ export function search(searchType,status){
     fetch('/recipe/search', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(requestBody)
     })
@@ -105,8 +103,12 @@ export function addRecipeAd(recipe) {
     h5Author.className = "recad-author";
     divTags.className = "recad-tags";
     divRecipeAd.addEventListener("click", function () {
-        //     tutaj chcemy przejść do szczegółow
-        window.alert("przechodzimy")
+        localStorage.setItem('recipe', recipe.id);
+        if(localStorage.getItem('username')!=null){
+            window.location.href = 'wyswietl-przepis.html';
+        }else{
+            window.location.href = 'wyswietl-przepis-niezalogowany.html';
+        }
     })
     //imgRecipe.src = recipe.image;
     imgRecipe.src = "img/example.jpg";
@@ -151,8 +153,181 @@ export function createTags(tags, divTags){
     }
 }
 
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('psw');
+const password2 = document.getElementById('psw2');
+export function openForm() {
+    document.getElementById("rejestracja").style.display = "block";
+}
+export function closeForm() {
+    document.getElementById("form").reset();
+    setDefault(username);
+    setDefault(email);
+    setDefault(password);
+    setDefault(password2);
+    document.getElementById("rejestracja").style.display = "none";
+}
+
+export function login(loginForm){
+    const formData = new FormData(loginForm);
+
+    fetch('/auth/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData))
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                message = response.getAllResponseHeaders()
+                throw new Error(message);
+            }
+        })
+        .then(data => {
+            console.log(data);
+            loginForm.reset();
+            const jwtToken = data.token;
+
+            localStorage.setItem('jwtToken', jwtToken);
+            localStorage.setItem('username', formData.get('username').toString());
+
+            window.location.href = 'homepage.html';
+        })
+        .catch(error => {
+            console.error(error.message);
+            alert('Niepoprawny login lub hasło');
+        });
+}
+
+export function register(e, registrationForm){
+    if(validate(e)){
+        const formData = new FormData(registrationForm);
+
+        const requestBody = {
+            username: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('psw')
+        };
+        fetch('/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log(response)
+                    alert('Gratulacje! Zarejestrowałeś się');
+                    closeForm();
+                } else {
+                    throw new Error('Registration failed');
+                }
+            })
+            .catch(error => {
+                console.error(error.message);
+                alert('Istnieje już użytkownik o takiej nazwie');
+                document.getElementById("form").reset();
+                setDefault(username);
+                setDefault(email);
+                setDefault(password);
+                setDefault(password2);
+            });
+    }
+}
 
 
+//Validating registration input
+const validate = (e) => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Podaj nazwę użytkownika');
+        return false;
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Podaj adres email');
+        return false;
+
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Email nieprawidłowy');
+        return false;
+
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Podaj hasło');
+        return false;
+
+    } else if (passwordValue.length < 6 ) {
+        setError(password, 'Hasło musi mieć przynjamniej 6 znaków.')
+        return false;
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Potwierdź hasło');
+        return false;
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Hasła są różne");
+        return false;
+    } else {
+        setSuccess(password2);
+    }
+    return true;
+};
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.err');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.err');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const setDefault = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.err');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.remove('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const searchForm= document.getElementById('searchForm')
+
+searchForm.addEventListener("submit", e =>  {
+    e.preventDefault();
+    search("short",true);
+});
 
 
 
--- src/main/resources/static/homepage.js
+++ src/main/resources/static/homepage.js
@@ -1,6 +1,6 @@
-import {search} from "./common";
+import {search} from "./common.js";
 
-const searchForm= document.getElementById('searchForm')
+const searchForm = document.getElementById('searchFrom')
 
 searchForm.addEventListener("submit", e =>  {
     e.preventDefault();
